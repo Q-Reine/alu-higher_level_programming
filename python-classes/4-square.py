2fcf4dbfce4eb79faeaf7d20b95ce16e0df81f51
Q-Reine <q.wihogora@alustudent.com>
2024-03-14 00:35:50 +0200

    Access and update private attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-""" Defines a square"""
+""" Defines a class Square"""
 
 
 class Square:
-    """ class square defines a square by size """
+     """Square represents an instance of a square shape"""
+
     def __init__(self, size=0):
         """ initializes the object square
           Args:
             size (int): size of a side of the square
         Returns: None
         """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
-    def area(self):
+        def area(self):
                 """calculates the area of a square"""
                 return (self.__size) ** 2
 
