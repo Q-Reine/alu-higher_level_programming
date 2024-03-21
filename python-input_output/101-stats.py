4a57586d5f588665eee434c20b36146dfdee60e4
Q-Reine <q.wihogora@alustudent.com>
2024-03-21 12:55:06 +0200

    Log parsing

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,36 +1,52 @@
 #!/usr/bin/python3
 """ reads stdin line by line and computes metrics"""
-import sys
 
-file_size = 0
-status_code = {"200": 0, "301": 0, "400": 0, "401": 0,
-               "403": 0, "404": 0, "405": 0, "500": 0}
-iter = 0
-try:
-    for line in sys.stdin:
-        iter += 1
-        line_list = line.split()
-        if len(line_list) >= 2:
-            if line_list[-2] in status_code:
-                status_code[line_list[-2]] += 1
+
+def print_stats(size, status_codes):
+    """Print accumulated metrics.
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
+
+
+if __name__ == "__main__":
+    import sys
+
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
+
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
             try:
-                file_size += int(line_list[-1])
-            except FileNotFoundError:
+                size += int(line[-1])
+            except (IndexError, ValueError):
                 pass
 
-        if iter % 10 == 0:
-            print(f"File size: {file_size:d}")
-            for key, value in sorted(status_code.items()):
-                if value:
-                    print(f"{key}: {value:d}")
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
 
-    print(f"File size: {file_size:d}")
-    for key, value in sorted(status_code.items()):
-        if value:
-            print(f"{key}: {value:d}")
+        print_stats(size, status_codes)
 
-except KeyboardInterrupt:
-    print(f"File size: {file_size:d}")
-    for key, value in sorted(status_code.items()):
-        if value:
-            print(f"{key}: {value:d}")
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
