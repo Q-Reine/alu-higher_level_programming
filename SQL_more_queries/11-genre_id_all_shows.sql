9815495fddf8ac76098018c7996346c9dc12d35a
Q-Reine <q.wihogora@alustudent.com>
2024-03-22 12:55:29 +0200

    Genre ID for all shows

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,5 @@
 -- Import the database dump of hbtn_0d_tvshows to your MySQL server
-SELECT tvs.title, tvsg.genre_id
-FROM tv_shows AS tvs
-INNER JOIN tv_show_genres AS tvsg
-ON tvsg.show_id = tvs.id
-ORDER BY tvs.title, tvsg.genre_id;
+SELECT s.title AS title, g.genre_id AS genre_id
+FROM tv_shows s LEFT OUTER JOIN tv_show_genres g
+  ON s.id = g.show_id
+ORDER BY s.title, g.genre_id;
