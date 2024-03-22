9aa67f4d9e24cc1cbdf85ed5ca47826b741e4fb7
Q-Reine <q.wihogora@alustudent.com>
2024-03-22 12:09:47 +0200

    Genre ID by show

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server
+SELECT tvs.title, tvsg.genre_id
+FROM tv_shows AS tvs
+INNER JOIN tv_show_genres AS tvsg
+ON tvsg.show_id = tvs.id
+ORDER BY tvs.title, tvsg.genre_id;
