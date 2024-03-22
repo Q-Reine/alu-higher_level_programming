26dddebd967e23b76d3833d0bb61a7bb95c09c66
Q-Reine <q.wihogora@alustudent.com>
2024-03-22 15:22:20 +0200

    cURL to the end

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+if [ "$(curl -sLI "$1" -X GET | grep "200 OK" | cut -d' ' -f2)" = '200' ]; then curl -sL "$1"; fi
