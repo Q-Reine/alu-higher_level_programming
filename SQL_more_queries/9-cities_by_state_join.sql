007590f7d523072075346c2ecd9a7b57a7a68148
Q-Reine <q.wihogora@alustudent.com>
2024-03-22 10:52:37 +0200

    Cities by States

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- Lists all cities contained in the database hbtn_0d_usa
-SELECT * cities.id, cities.name
-FROM cities
-WHERE cities.state_id
-ORDER BY cities.id;
+SELECT * c.id, c.name, s.name
+FROM cities AS c INNER JOIN states AS s
+ON c.state_id = s.id
+ORDER BY c.id;
+
