3023b4a7887021a1f8df028f706b7d5002d26cda
Q-Reine <q.wihogora@alustudent.com>
2024-03-23 22:32:37 +0000

    What's my status? #0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = resp.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
