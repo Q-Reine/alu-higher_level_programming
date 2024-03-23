44cea6c2f27e7b024e499cfcba80ad6969398ab7
Q-Reine <q.wihogora@alustudent.com>
2024-03-23 20:26:06 +0000

    What's my status? #0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that fetches https://alu-intranet.hbtn.io/status"""
+""" Script that fetches https://alu-intranet.hbtn.io/statu s"""
 import urllib.request
 
 
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(request) as response:
     r = response.read()
     print('Body response:')
-    print('\t type: {}'.format(type(r)))
-    print('\t content: {}'.format(r))
-    print('\t utf8 content: {}'.format(r.decode('UTF-8')))
+    print('\t- type: {}'.format(type(r)))
+    print('\t- content: {}'.format(r))
+    print('\t- utf8 content: {}'.format(r.decode('UTF-8')))
