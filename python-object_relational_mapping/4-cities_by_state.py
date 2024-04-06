f74a7c8686481274e3a94be4d0ae2ddecf610046
Q-Reine <q.wihogora@alustudent.com>
2024-04-06 13:44:44 -0700

    Cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
 
-    c = database.cursor()
+    c = db.cursor()
     c.execute("""SELECT cities.id, cities.name, states.name
                FROM cities INNER JOIN states
                ON states.id = cities.state_id
